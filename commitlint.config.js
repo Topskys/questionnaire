1f139005dfd66f813cb02b5bee1fe495bcac34a0
Topskys <2477350343@qq.com>
2024-08-08 16:27:48 +0800

    chore: 配置commitizen规范message

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,7 @@
-module.exports= { extends: ['@commitlint/config-conventional'] };
+module.exports= { 
+    extends: ['@commitlint/config-conventional'],
+    // rules: {
+    // 要求commit信息中包含scope括号，like： feat(scope): add new feature
+    //     "scope-empty": [2, "never"],
+    // }
+ };
