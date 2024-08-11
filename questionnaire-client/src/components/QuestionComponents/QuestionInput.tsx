6007e8392cf92187f738ec826cfe99d200623e8b
Topskys <2477350343@qq.com>
2024-08-11 22:10:58 +0800

    feat: c端新增输入框和单选框组件

---
+++ questionnaire-client/src/components/QuestionComponents/QuestionInput.tsx
@@ -0,0 +1,24 @@
+import React, { FC } from 'react'
+import styles from './QuestionInput.module.scss'
+
+type PropsType = {
+  fe_id: string // 组件id
+  props: {
+    title: string // 标题
+    placeholder?: string // 占位符
+  }
+}
+
+const QuestionInput: FC<PropsType> = ({ fe_id, props }) => {
+  const { title, placeholder = '' } = props
+  return (
+    <>
+      <p>{title}</p>
+      <div className={styles['input-wrapper']}>
+        <input name={fe_id} placeholder={placeholder} />
+      </div>
+    </>
+  )
+}
+
+export default QuestionInput
---
+++ questionnaire-client/src/components/QuestionComponents/QuestionRadio.tsx
@@ -0,0 +1,51 @@
+import React, { FC } from 'react'
+import styles from './QuestionRadio.module.scss'
+
+type PropsType = {
+  fe_id: string // 组件id
+  props: {
+    title: string // 标题
+    options: Array<{
+      text: string
+      value: string
+    }> // 选项
+  }
+  value: string // 当前选中值
+  isVertical?: boolean // 是否垂直排列
+}
+
+const QuestionRadio: FC<PropsType> = ({ fe_id, value, isVertical, props }) => {
+  const { title, options = [] } = props
+  return (
+    <>
+      <p>{title}</p>
+      <ul className={styles.list}>
+        {options.map(o => {
+          const { text, value: val } = o
+
+          // 判断是否垂直排列
+          let liClassName = ''
+          if (isVertical) liClassName = styles.verticalItem
+          else liClassName = styles.horizontalItem
+
+          return (
+            <li key={val} className={liClassName}>
+              <label>
+                <input
+                  type="radio"
+                  name={fe_id}
+                  value={value}
+                  defaultChecked={val === value}
+                  onChange={() => {}}
+                />
+                {text}
+              </label>
+            </li>
+          )
+        })}
+      </ul>
+    </>
+  )
+}
+
+export default QuestionRadio
